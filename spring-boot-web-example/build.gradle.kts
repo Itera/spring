70cf004f7a6a8eed550e60cc95ab1ad8096f2dec
Chris Searle <chris@chrissearle.org>
2022-03-10 15:46:29 +0100

    Bump kotlin version to 1.5.x

--- spring-boot-web-example/build.gradle.kts
+++ spring-boot-web-example/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 	id("org.springframework.boot") version "2.4.2"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 
-	kotlin("jvm") version "1.4.21"
-	kotlin("plugin.spring") version "1.4.21"
+	kotlin("jvm") version "1.5.31"
+	kotlin("plugin.spring") version "1.5.31"
 }
 
 group = "no.itera"
